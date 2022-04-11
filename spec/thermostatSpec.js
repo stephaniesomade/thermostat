38412dc5d179c14c32324fb646b41a204dca8399
Stephanie <stephaniesomadee@gmail.com>
2022-04-11 12:14:01 +0100

    Jasmine Setup

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,9 @@
+'use strict';
+// So as to ensure clear tests
+describe('Thermostat', () => {
+// for the Thermostat class
+// This is what is created before each new test. 
+beforeEach(() => {
+  thermostat = new Thermostat();
+}
+});
