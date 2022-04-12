19d1e80cbe6eb77870b9118d02a5673acee515c5
stephaniesomade <stephaniesomadee@gmail.com>
2022-04-12 23:50:41 +0100

    Added some basic DOM functions

---
+++ src/interface.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const updateTemperature = () => {
+    document.querySelector('#temperature').innerText = thermostat.temperature;
+    document.querySelector('#temperature').className = thermostat.energyUsage();
+    }
+
+  const thermostat = new Thermostat();
+  updateTemperature();
+
+  document.querySelector('#temperature-up').addEventListener('click', () => {
+    thermostat.up();
+    updateTemperature();
+  });
+
+  document.querySelector('#temperature-down').addEventListener('click', () => {
+    thermostat.down();
+    updateTemperature();
+  });
+
+  document.querySelector('#temperature-reset').addEventListener('click', () => {
+    thermostat.resetTemperature();
+    updateTemperature();
+  });
+
+  document.querySelector('#powersaving-on').addEventListener('click', () => {
+    thermostat.switchPowerSavingModeOn();
+    document.querySelector('#power-saving-status').innerText = "on";
+    updateTemperature();
+  });
+
+  document.querySelector('#powersaving-off').addEventListener('click', () => {
+    thermostat.switchPowerSavingModeOff();
+    document.querySelector('#power-saving-status').innerText = 'off';
+    updateTemperature();
+  });
+});
