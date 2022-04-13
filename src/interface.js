3232f4edfd852d658b330ea0ae379bd5008e92eb
stephaniesomade <stephaniesomadee@gmail.com>
2022-04-13 13:45:44 +0100

    added weather API

--- src/interface.js
+++ src/interface.js
@@ -7,6 +7,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const thermostat = new Thermostat();
   updateTemperature();
 
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    document.querySelector('#current-temperature').innerText = data.main.temp;
+  });
+
   document.querySelector('#temperature-up').addEventListener('click', () => {
     thermostat.up();
     updateTemperature();
@@ -32,5 +40,5 @@ document.addEventListener("DOMContentLoaded", () => {
     thermostat.switchPowerSavingModeOff();
     document.querySelector('#power-saving-status').innerText = 'off';
     updateTemperature();
-  });
+  });  
 });
